837e7c9eb284d336cde739f3a5be59da282af3c9
Ishak <ishaksmajicpac@gmail.com>
2025-11-07 08:31:11 +0100

    Did the logic for the todo list

---
+++ src/components/todoForm.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export default function TodoForm({addTodo}){
+    const [text, setText] = useState("");
+
+function handleSubmit(e){
+    e.preventDefault();
+    if (!text.trim()) return;
+    addTodo(text);
+    setText("");
+}
+
+return (
+    <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Add a new task"/>
+        <button type="submit">Add</button>
+    </form>
+);
+}
---
+++ src/components/todoList.jsx
@@ -0,0 +1,13 @@
+import TodoItem from "./TodoItem"
+
+export default function TodoList({ todos, toggleTodo, deleteTodo }){
+    if(todos.length === 0) return <p>No tasks yet.</p>;
+
+    return(
+        <ul>
+            {todos.map((todo) => (
+                <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+            ))}
+        </ul>
+    );
+}
